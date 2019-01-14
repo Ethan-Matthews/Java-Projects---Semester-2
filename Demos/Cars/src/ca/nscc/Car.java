8fca3156455ccf4f9667d81e2fbf23f462b6be62
Ethan <0420789@nscc.ca>
2019-01-14 18:27:38 -0400

    Testing Home Desktop Connection with IntelliJ.

---
+++ Demos/Cars/src/ca/nscc/Car.java
@@ -0,0 +1,34 @@
+package ca.nscc;
+
+public class Car {
+    //Here we can define what a car looks like and acts like in a program.
+
+    //Property Variables.
+    String make;
+    String model;
+    int manufYear;
+    int topSpeed;
+    String color;
+
+    //methods
+    public void moveForward() {
+
+    }
+
+    public void moveBackward() {
+
+    }
+
+    public void turnLeft() {
+
+    }
+
+    public void turnRight() {
+
+    }
+
+    public void slowBreak() {
+
+    }
+
+}
