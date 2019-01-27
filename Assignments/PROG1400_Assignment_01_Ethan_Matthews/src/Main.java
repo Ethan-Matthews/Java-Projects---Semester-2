783f6737911dba6c65de1e8545821da98270a530
Ethan Matthews <w0420789@nscc.ca>
2019-01-27 15:00:08 -0400

    v1.4

--- Assignments/PROG1400_Assignment_01_Ethan_Matthews/src/Main.java
+++ Assignments/PROG1400_Assignment_01_Ethan_Matthews/src/Main.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,6 +15,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int minimumChar = 3;
+        int minimumInt = 0;
         int teamCount = 3;
         int i = 1;
         int i1 = 0;
@@ -25,7 +24,7 @@ public class Main {
             System.out.println("Enter name for team # " + i + ": ");
             String currentTeam = sc.nextLine();
             if (currentTeam.length() <= minimumChar) {
-                System.out.println("ERROR!!! The team name must be longer. Please reenter a team name.");
+                System.out.println("ERROR!!! The team name must be longer. Please re-enter a team name.");
                 continue;
             }
 
@@ -35,9 +34,75 @@ public class Main {
             i1++;
         }
 
+        System.out.println("PLAYER ENTRY");
+        System.out.println("==============================");
+
+        Player[] firstTeamArray = new Player[4];
+        Player[] secondTeamArray = new Player[4];
+        Player[] thirdTeamArray = new Player[4];
 
 
 
+        int i2 = 1;
+        int i3 = 0;
+
+        while ( i2 <= teamCount ) {
+            System.out.println("Enter players for " + teamArray[i3].Name + ":");
+
+            for (i = 0; i <= firstTeamArray.length;) {
+                System.out.println("Enter name for player # " + (i + 1) + ":");
+                String currentPlayer = sc.nextLine();
+
+                if (currentPlayer.length() <= minimumChar) {
+                    System.out.println("ERROR!!! The player name must be longer. Please re-enter a player info.");
+                    continue;
+                }
+
+                System.out.println("Enter number of goals for " + currentPlayer + ":");
+                int currentGoals = sc.nextInt();
+                if (currentGoals <= minimumInt) {
+                    System.out.println("ERROR!!! The player goals must be greater than zero. Please re-enter player info.");
+                    continue;
+                }
+
+                System.out.println("Enter number of assists for " + currentPlayer + ":");
+                int currentAssists = sc.nextInt();
+                if (currentAssists <= minimumInt) {
+                    System.out.println("ERROR!!! The player assists must be greater than zero. Please re-enter player info.");
+                    continue;
+                }
+
+                if ( i2 == 1 ) {
+                    firstTeamArray[i] = new Player();
+                    firstTeamArray[i].Name = currentPlayer;
+                    firstTeamArray[i].Goals = currentGoals;
+                    firstTeamArray[i].Assists = currentAssists;
+
+                }
+
+                else if ( i2 == 2 ) {
+                    secondTeamArray[i] = new Player();
+                    secondTeamArray[i].Name = currentPlayer;
+                    secondTeamArray[i].Goals = currentGoals;
+                    secondTeamArray[i].Assists = currentAssists;
+
+                }
+
+                else if ( i2 == 3 ) {
+                    thirdTeamArray[i] = new Player();
+                    thirdTeamArray[i].Name = currentPlayer;
+                    thirdTeamArray[i].Goals = currentGoals;
+                    thirdTeamArray[i].Assists = currentAssists;
+
+                }
+
+                i++; //for loop
+
+            }
+
+            i2++; //while 2 loop
+            i3++;//while 2 loop
+        }
 
 
 
--- Assignments/PROG1400_Assignment_01_Ethan_Matthews/src/Player.java
+++ Assignments/PROG1400_Assignment_01_Ethan_Matthews/src/Player.java
@@ -1,2 +1,14 @@
 public class Player {
+
+    //Constructor.
+    String Name;
+    int Goals;
+    int Assists;
+    public Player () {
+        this.Name = Name;
+        this.Goals = Goals;
+        this.Assists = Assists;
+
+    }
+
 }
