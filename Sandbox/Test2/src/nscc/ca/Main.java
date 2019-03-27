a9efac5064c5b25e4db538ea874651721a505db9
Ethan Matthews <w0420789@nscc.ca>
2019-03-27 14:55:32 -0300

    v1.2

---
+++ Sandbox/Test2/src/nscc/ca/Main.java
@@ -0,0 +1,10 @@
+package nscc.ca;
+
+import nscc.ca.Core.Game;
+import nscc.ca.Core.Window;
+
+public class Main {
+
+    public static void main(String[] args) { new Window(1920 , 1080, "SideScroller/Platformer", new Game());
+    }
+}
